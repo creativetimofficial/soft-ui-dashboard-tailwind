3940f786c75d344af5b133d177c64468de115463
Stefan <stefan@creative-tim.com>
2022-08-04 23:07:54 +0300

    refactor: update product live preview to v1.0.4

--- assets/js/fixed-plugin.js
+++ assets/js/fixed-plugin.js
@@ -19,7 +19,7 @@ var transparentBtn = document.querySelector("[transparent-style-btn]");
 var whiteBtn = document.querySelector("[white-style-btn]");
 
 var non_active_style = ["bg-none", "bg-transparent", "text-fuchsia-500", "border-fuchsia-500"];
-var active_style = ["bg-gradient-fuchsia", "bg-fuchsia-500", "text-white", "border-transparent"];
+var active_style = ["bg-gradient-to-tl", "from-purple-700", "to-pink-500", "bg-fuchsia-500", "text-white", "border-transparent"];
 
 var transparent_sidenav_classes = ["xl:bg-transparent", "shadow-none"];
 var transparent_sidenav_highlighted = ["shadow-soft-xl"];
@@ -83,31 +83,48 @@ if (pageName != "rtl") {
 // color sidenav
 
 function sidebarColor(a) {
-  var color = a.getAttribute("data-color");
+  var color_from = a.getAttribute("data-color-from");
+  var color_to = a.getAttribute("data-color-to");
   var parent = a.parentElement.children;
-  var activeColor;
-  var activeSidenavIconColorClass;
-  var activeSidenavCardColorClass;
-  var activeSidenavCardIconColorClass;
-
-  var checkedSidenavIconColor = "bg-gradient-" + color;
-  var checkedSidenavCardColor = "after:bg-gradient-" + (color == "fuchsia" ? "slate" : color);
-  var checkedSidenavCardIconColorClass = "bg-gradient-" + (color == "fuchsia" ? "slate" : color);
+  
+  var activeColorFrom;
+  var activeColorTo;
+  var activeSidenavIconColorClassFrom;
+  var activeSidenavIconColorClassTo;
+  var activeSidenavCardColorClassFrom;
+  var activeSidenavCardColorClassTo;
+  var activeSidenavCardIconColorClassFrom;
+  var activeSidenavCardIconColorClassTo;
+  
+  var checkedSidenavIconColorFrom = "from-" + color_from;
+  var checkedSidenavIconColorTo = "to-" + color_to;
+  
+  var checkedSidenavCardColorFrom = "after:from-" + (color_from == "purple-700" ? "slate-600" : color_from);
+  var checkedSidenavCardColorTo = "after:to-" + (color_to == "pink-500" ? "slate-300" : color_to);
+  
+  var checkedSidenavCardIconColorClassFrom = "from-" + (color_from == "purple-700" ? "slate-600" : color_from);
+  var checkedSidenavCardIconColorClassTo = "to-" + (color_to == "pink-500" ? "slate-300" : color_to);
 
   var sidenavCard = document.querySelector("[sidenav-card]");
   var sidenavCardIcon = document.querySelector("[sidenav-card-icon]");
-  var sidenavIcon = document.querySelector("a[href=" + CSS.escape(sidenav_target) + "]").firstElementChild;
+  var sidenavIcon = sidenav_highlight.firstElementChild;
 
   for (var i = 0; i < parent.length; i++) {
     if (parent[i].hasAttribute("active-color")) {
-      activeColor = parent[i].getAttribute("data-color");
+      activeColorFrom = parent[i].getAttribute("data-color-from");
+      activeColorTo = parent[i].getAttribute("data-color-to");
 
       parent[i].classList.toggle("border-white");
       parent[i].classList.toggle("border-slate-700");
+      
+      activeSidenavIconColorClassFrom = "from-" + activeColorFrom;
+      activeSidenavIconColorClassTo = "to-" + activeColorTo;
 
-      activeSidenavIconColorClass = "bg-gradient-" + activeColor;
-      activeSidenavCardColorClass = "after:bg-gradient-" + (activeColor == "fuchsia" ? "slate" : activeColor);
-      activeSidenavCardIconColorClass = "bg-gradient-" + (activeColor == "fuchsia" ? "slate" : activeColor);
+      activeSidenavIconColorClassFrom = "from-" + activeColorFrom;
+      activeSidenavIconColorClassTo = "to-" + activeColorTo;
+
+      activeSidenavCardIconColorClassFrom = "from-" + (activeColorFrom == "purple-700" ? "slate-600" : activeColorFrom);
+      activeSidenavCardIconColorClassTo = "to-" + (activeColorTo == "pink-500" ? "slate-300" : activeColorTo);
     }
     parent[i].removeAttribute("active-color");
   }
@@ -118,17 +135,23 @@ function sidebarColor(a) {
   a.classList.toggle("border-white");
   a.classList.toggle("border-slate-700");
 
-  //   remove active style
-
-  sidenavCard.classList.remove(activeSidenavCardColorClass);
-  sidenavCardIcon.classList.remove(activeSidenavCardIconColorClass);
-  sidenavIcon.classList.remove(activeSidenavIconColorClass);
-
-  //   add new style
-
-  sidenavCard.classList.add(checkedSidenavCardColor);
-  sidenavCardIcon.classList.add(checkedSidenavCardIconColorClass);
-  sidenavIcon.classList.add(checkedSidenavIconColor);
+  sidenavCard.classList.remove(activeSidenavCardColorClassFrom);
+  sidenavCard.classList.remove(activeSidenavCardColorClassTo);
+  
+  sidenavCardIcon.classList.remove(activeSidenavCardIconColorClassFrom);
+  sidenavCardIcon.classList.remove(activeSidenavCardIconColorClassTo);
+  
+  sidenavIcon.classList.remove(activeSidenavIconColorClassFrom);
+  sidenavIcon.classList.remove(activeSidenavIconColorClassTo);
+  
+  sidenavCard.classList.add(checkedSidenavCardColorFrom);
+  sidenavCard.classList.add(checkedSidenavCardColorTo);
+  
+  sidenavCardIcon.classList.add(checkedSidenavCardIconColorClassFrom);
+  sidenavCardIcon.classList.add(checkedSidenavCardIconColorClassTo);
+  
+  sidenavIcon.classList.add(checkedSidenavIconColorFrom);
+  sidenavIcon.classList.add(checkedSidenavIconColorTo);
 }
 
 // sidenav style
